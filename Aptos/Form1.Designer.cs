2ca9499b0e1653babbb473c20a5bdbcd1be1e109
Marsell3dll <marsel.smakov@mail.ru>
2022-04-17 01:29:00 +0300

    Добавьте файлы проекта.

---
+++ Aptos/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Aptos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer_Tick1);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Font = new System.Drawing.Font("SIMPLEX Regular", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(341, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "APTOS tokens: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.SystemColors.WindowText;
+            this.scoreLabel.Font = new System.Drawing.Font("SIMPLEX Regular", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.scoreLabel.ForeColor = System.Drawing.Color.Cornsilk;
+            this.scoreLabel.Location = new System.Drawing.Point(323, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(42, 37);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Aptos.Resource1.Background;
+            this.ClientSize = new System.Drawing.Size(801, 405);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MAINNING :D";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ Aptos/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aptos
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap HandlerTexture=Resource1.Handler,
+            TargetTexture = Resource1.Target;
+
+        private Point _TargetPosition = new  Point(200,200);
+
+        private Point _direction = Point.Empty;
+        int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick1(object sender, EventArgs e)
+        {
+            Refresh();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | 
+                ControlStyles.AllPaintingInWmPaint | 
+                ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(10, 10000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _TargetPosition.X += _direction.X*10;
+            _TargetPosition.Y += _direction.Y*10;
+
+            if(_TargetPosition.X < 0 ||_TargetPosition.X > 800 )
+            {
+                _direction.X *= -1;
+                
+            }
+            if(_TargetPosition.Y <0 ||  _TargetPosition.Y > 390)
+            {
+                _direction.Y *= -1;
+;
+            }
+
+            Point between = new Point(localPosition.X - _TargetPosition.X, localPosition.Y - _TargetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+            if (distance < 20)
+            {
+                AddScore(1);
+            }
+
+
+            var handlerRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_TargetPosition.X- 50, _TargetPosition.Y - 50, 100, 100);
+            g.DrawImage(HandlerTexture, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
+        }
+
+        //private void scoreLabel_Click(object sender, EventArgs e)
+        //{
+        //}
+
+        private void AddScore (int score)
+        {
+            _score += score;
+            scoreLabel.Text = _score.ToString();
+        }
+    }
+}
